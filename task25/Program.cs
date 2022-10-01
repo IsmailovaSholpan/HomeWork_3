f859ecb3a6d84ba1aba1a66bcf8d89797240230d
[sholpan] <[sholpanismailova@gmail.com]>
2022-10-01 23:50:17 +0600

    added homework4

---
+++ task25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень числа: ");
+int step = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+int pow = 1;
+if (step <= 0)
+{
+    Console.WriteLine("Степень числа должна быть выше 0");
+}
+else
+{
+    while (count <= step)
+    {
+        
+        pow = pow * number;
+        count = count + 1;
+    }
+Console.WriteLine($"{number}, {step} -> {pow}");}
---
+++ task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = number;
+int sum = 0;
+
+if (number < 0)
+{ Console.WriteLine($"Число должно быть больше 0"); }
+else
+{
+    while (count > 0)
+    {
+        sum = sum + count % 10;
+        count = count / 10;
+    }
+    Console.WriteLine($"{number} -> {sum}");
+};
